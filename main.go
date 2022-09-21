472909af937fa5f06c9e4e8d8255adc2bdd5fde4
aldi <aldi.sptra86@gmail.com>
2022-09-21 11:35:08 +0700

    first-commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	port := "7177"
+	handler := func(w http.ResponseWriter, req *http.Request) {
+		i := 1
+		response := ""
+		for {
+			if i > 10 {
+				break
+			}
+			multiplication := `99 x ` + strconv.Itoa(i) + ` = ` + strconv.Itoa(99*i) + ``
+			response += multiplication
+			response += "\n"
+			i++
+		}
+		io.WriteString(w, response)
+	}
+
+	http.HandleFunc("/api/multiplication99", handler)
+	log.Println("Listing for :" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
